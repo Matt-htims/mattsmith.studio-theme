7d4322c6a53414a49dd0efa1551c1b1f7c5ff608
Matt Smith <matt.smith3108@gmail.com>
2022-08-01 12:47:39 +0100

    initial styling for triple-explainer

--- template-parts/blocks/triple-explainer/code.php
+++ template-parts/blocks/triple-explainer/code.php
@@ -23,31 +23,33 @@
         <div class="te-text">
 		    
             <?php while ( have_rows('rows') ) : the_row(); ?>
+
+                <div class="te-text-outer">
 		    
-		    	<div class="te-text-indiv">
-		    	
-		    		<?php if(get_sub_field('image')) { ?>
-		    			        			    	
-		    			<?php $attachment_id = get_sub_field('image');
-		    			$size = "largeSquare";
-		    			$image = wp_get_attachment_image_src( $attachment_id, $size ); ?>
-		    			<img src="<?php echo $image[0]; ?>" />
-		    		
-		    		<?php } ?>
-		    		
-		    	</div>
+                    <div class="te-text-image">
+                    
+                        <?php if(get_sub_field('image')) { ?>
+                                                        
+                            <?php $attachment_id = get_sub_field('image');
+                            $size = "largeSquare";
+                            $image = wp_get_attachment_image_src( $attachment_id, $size ); ?>
+                            <img src="<?php echo $image[0]; ?>" />
+                        
+                        <?php } ?>
+                        
+                    </div>
 
-		    	<div class="text">
-		    	
-		    		<h3><?php the_sub_field('row_title'); ?></h3>
+                    <div class="te-text-text">
+                    
+                        <h3><?php the_sub_field('row_title'); ?></h3>
 
-		    		<?php the_sub_field('row_copy'); ?>
+                        <p class="te-text-main"><?php the_sub_field('row_copy'); ?></p>
 
-                    <?php the_sub_field('row_bottom') ?>
-		    		
-		    	</div><!-- .pic-text-text -->
+                        <p class="te-text-sub"><?php the_sub_field('row_bottom') ?></p>
+                        
+                    </div><!-- .pic-text-text -->
 		    	
-		    
+		        </div>
 
 		    <?php endwhile; ?>
 		
